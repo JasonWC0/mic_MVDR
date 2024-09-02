632f723a01eb4db297aad85c6a87d5eebc59951e
z6259tw <z6259tw@gmail.com>
2024-09-02 22:16:03 +0800

    feat: segment

--- src/AUTO_SPLIT_SIGNAL_TO_PARTS.py
+++ src/AUTO_SPLIT_SIGNAL_TO_PARTS.py
@@ -5,10 +5,11 @@ from scipy.signal import hilbert
 import os
 
 # 讀取 CSV 文件
-file_path = './RAW_TDM_DATA/mic1/1.csv'
-output_folder_mic1_template = './LONG_TDM_{}/mic1/'
-output_folder_mic2_template = './LONG_TDM_{}/mic2/'
-output_folder_mic3_template = './LONG_TDM_{}/mic3/'
+file_path = './S6500_5/1.csv'
+START_INDEX=0
+output_folder_mic1_template = './LONG_TDM_1/mic1/'
+output_folder_mic2_template = './LONG_TDM_1/mic2/'
+output_folder_mic3_template = './LONG_TDM_1/mic3/'
 
 mic1_data = pd.read_csv(file_path, usecols=[0, 1], header=None, names=['Time', 'Amplitude'])
 mic2_data = pd.read_csv(file_path, usecols=[0, 2], header=None, names=['Time', 'Amplitude'])
@@ -62,39 +63,39 @@ for peak_time in peak_times:
         segments3.append(segment3)
     
 
-# 畫出整個包絡線和原始數據
-plt.figure(figsize=(15, 6))
-plt.plot(time, amplitude, color='lightblue', label='Original Signal')
-plt.plot(time, envelope, color='blue', label='Envelope')
+# # 畫出整個包絡線和原始數據
+# plt.figure(figsize=(15, 6))
+# plt.plot(time, amplitude, color='lightblue', label='Original Signal')
+# plt.plot(time, envelope, color='blue', label='Envelope')
 
-# 在每個峰值點處繪製垂直線
-for peak_time in peak_times:
-    plt.axvline(x=peak_time, color='red', linestyle='--', linewidth=2)
+# # 在每個峰值點處繪製垂直線
+# for peak_time in peak_times:
+#     plt.axvline(x=peak_time, color='red', linestyle='--', linewidth=2)
 
-plt.xlabel('Time')
-plt.ylabel('Amplitude')
-plt.title('Original Signal and Envelope with Peak-Based Segments')
-plt.legend()
-plt.grid(True)
-#plt.show()
+# plt.xlabel('Time')
+# plt.ylabel('Amplitude')
+# plt.title('Original Signal and Envelope with Peak-Based Segments')
+# plt.legend()
+# plt.grid(True)
+# plt.show()
 
 # 保存每個片段為單獨的 CSV 文件，並畫出每個片段的波形和包絡線
 for i, segment in enumerate(segments1):
     output_folder = os.path.join(output_folder_mic1_template.format(i))
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
-    segment[['Time', 'Amplitude']].to_csv(output_folder + f'wear1-1_timeDM.csv', index=False)
+    segment[['Time', 'Amplitude']].to_csv(output_folder + f'wear{i+1+START_INDEX}-1_timeDM.csv', index=False)
 
 for i, segment in enumerate(segments2):
     output_folder = os.path.join(output_folder_mic2_template.format(i))
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
-    segment[['Time', 'Amplitude']].to_csv(output_folder + f'wear1-2_timeDM.csv', index=False)
+    segment[['Time', 'Amplitude']].to_csv(output_folder + f'wear{i+1+START_INDEX}-2_timeDM.csv', index=False)
 
 for i, segment in enumerate(segments3):
     output_folder = os.path.join(output_folder_mic3_template.format(i))
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
-    segment[['Time', 'Amplitude']].to_csv(output_folder + f'wear1-3_timeD.csv', index=False)
+    segment[['Time', 'Amplitude']].to_csv(output_folder + f'wear{i+1+START_INDEX}-3_timeDM.csv', index=False)
 
 print("包絡計算、切割和繪圖完成，並保存到不同的 CSV 文件中。")
